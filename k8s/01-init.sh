270ef2afa737e2e0860ef6245591be35b2f48809
root <root@ip-172-31-87-140.ec2.internal>
2018-12-12 20:00:14 +0000

    update

---
+++ k8s/01-init.sh
@@ -0,0 +1,5 @@
+apt-get update && apt-get upgrade
+mkdir -p ~/k8s/
+cd ~/k8s
+wget https://github.com/kubernetes/kubernetes/releases/download/v1.13.0/kubernetes.tar.gz
+#https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
---
+++ k8s/02-start.sh
@@ -0,0 +1,1 @@
+#https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
